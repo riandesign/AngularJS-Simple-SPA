cfb885f251414aeff71a196eaf4d23f17d70264d
Rian Dutra <contato@riandesign.com.br>
2017-09-13 10:43:44 -0300

    All app files updated

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@
 // Dependencies: ngRoute
 var app = angular.module('app', ['ngRoute']);
 
+// App routing configuration
 app.config(function($routeProvider) {
   $routeProvider
     .when('/jobs', {
@@ -17,6 +18,46 @@ app.config(function($routeProvider) {
     redirectTo: '/jobs'
   });
 });
+
+
+// Dependency injection
+app.factory('jobService', function($http, $window) {
+
+  var selectedJob = null;
+  var all, items = [];
+
+  var loadJobs = function() {
+      return $http.get('data/jobs.json').then(function(response) {
+        all = response.data.items;
+
+        angular.forEach(all, function(x, i) {
+          if (i % 2 == 1) {
+            items.push(x);
+          }
+        });
+
+        return items;
+      });
+  }
+
+  return {
+    getJobs: loadJobs,
+    selectJob: function(job) {
+      //console.log('Job: ' + job.title);
+      selectedJob = job;
+    },
+    getSelectedJob: function() {
+      if (selectedJob == null) {
+        $window.location.href = '#/jobs';
+        return;
+      } else {
+        return selectedJob;
+      }
+    }
+  }
+
+});
+
 
 // Header controller
 app.controller('AppController', function($scope) {
@@ -35,19 +76,32 @@ app.controller('AppController', function($scope) {
   };
 });
 
+
 // Jobs controller
-app.controller('JobsController', function($scope) {
-  $scope.jobs = [
-    {
-      title: "First job"
-    },
-    {
-      title: "Second job"
-    },
-  ]
+// Without Dependency Injection
+/*
+app.controller('JobsController', function($scope, jobService) {
+  //var jsonUri = 'http://riandesign.com.br/galeria/albuns/json_output';
+  $http.get('data/jobs.json').then(function(response) {
+    //console.log(response.data);
+    $scope.jobs = response.data.items;
+  });
 });
+*/
+
+// Jobs Controller
+app.controller('JobsController', ['$scope','jobService',function($scope, jobService) {
+  jobService.getJobs().then(function(response){
+    $scope.jobs = response;
+  });
+
+  $scope.selectJob = function(job) {
+    jobService.selectJob(job);
+  }
+}]);
+
 
 // Job details controller
-app.controller('JobDetailsController', function($scope) {
-
+app.controller('JobDetailsController', function($scope, jobService) {
+  $scope.selectedJob = jobService.getSelectedJob();
 });
